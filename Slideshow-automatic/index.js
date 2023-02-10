c075e60a0d2ef8c81abe91c9a84b0cfcec4841a4
Aditya Bellary <adityabellary71@gmail.com>
2023-02-10 11:01:50 +0530

    slideshow-auto

---
+++ Slideshow-automatic/index.js
@@ -0,0 +1,13 @@
+let slideIndex = 0
+showSlides()
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides")
+    for (var i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    slideIndex++
+    if(slideIndex >= slides.length) {slideIndex = 0}
+    slides[slideIndex].style.display = "block"
+    setTimeout(showSlides, 2000)
+}
